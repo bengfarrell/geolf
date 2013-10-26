494c441becf9238a69cd9abebf3fa82201df0ed9
Ben Farrell <ben@benfarrell.com>
2013-10-26 16:41:23 -0700

    allow use of setInterval for allowing animation if requestAnimationFrame isn't available

--- app/scripts/services/animation.js
+++ app/scripts/services/animation.js
@@ -1,6 +1,13 @@
 app.service('animation', function() {
     var self = this;
 
+    /** should we try to use request animation frame? -
+     *  unavailable on mobile webviews and some browsers*/
+    this.useRequestAnimationFrame = false;
+
+    /** tick interval in milliseconds if using setInterval for animation timing */
+    this.tickInterval = 25;
+
     /**
      * start animation
      * @param animation
@@ -14,7 +21,12 @@ app.service('animation', function() {
         self.pause = 0;
         self.framecb = framecallback;
         self.animcb = animationcallback;
-        self._drawFrame();
+        if (requestAnimationFrame && self.useRequestAnimationFrame) {
+            self._drawFrame();
+        } else {
+            self.useRequestAnimationFrame = false;
+            self.tick = setInterval(self._drawFrame, self.tickInterval);
+        }
     }
 
     /**
@@ -24,23 +36,30 @@ app.service('animation', function() {
     this._drawFrame = function() {
         if (self.pause > 0) {
             self.pause --;
-            requestAnimationFrame(self._drawFrame);
+            if (self.useRequestAnimationFrame) {
+                requestAnimationFrame(self._drawFrame);
+            }
             return;
         }
         if (self.frames.length == 0) {
             if (self.animcb) {
                 self.animcb.apply(this);
             }
+            clearInterval(self.tick);
             return;
         }
         var f = self.frames.pop();
         if (f.pause) {
             self.pause = f.pause-1;
-            requestAnimationFrame(self._drawFrame);
+            if (self.useRequestAnimationFrame) {
+                requestAnimationFrame(self._drawFrame);
+            }
             return;
         }
 
         self.framecb.apply(this, [self.target, self.targetref, f]);
-        requestAnimationFrame(self._drawFrame);
+        if (self.useRequestAnimationFrame) {
+            requestAnimationFrame(self._drawFrame);
+        }
     }
 });
